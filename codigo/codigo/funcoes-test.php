ce6b3b218274aa5e27aa3b1486c09ae490ba3a5d
matheusf318 <0001063850@senaimgaluno.com.br>
2025-08-05 20:12:27 -0300

    Create funcoes-test.php

---
+++ codigo/codigo/funcoes-test.php
@@ -0,0 +1,5 @@
+<?php
+ Para testar a função soma($a, $b), eu chamaria a função com diferentes pares de valores
+ e verificaria se o retorno está correto. Por exemplo, soma(2, 3) deve retornar 5.
+ Também testaria com valores negativos e zero, como soma(-1, 1) ou soma(0, 0).
+?>
